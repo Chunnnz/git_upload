387707d6be9bba30dd34554f1d5ab5b2de8f1d6f
Chun_00550353 <jessie152545@gmail.com>
2024-08-22 10:47:24 +0800

    20240822

---
+++ JS/HashMapRevised.js
@@ -0,0 +1,31 @@
+
+let HashMap = function () {
+    let map = {};
+
+    return {
+        put: function (key, value) {
+            map[key] = value;
+        },
+
+        keys: function () {
+            let arr = [];
+            for (let key in map) {
+                arr.push(key);
+            }
+            return arr;
+        },
+
+        contains: function (key) {
+            return key in map;
+        },
+
+        get: function (key) {
+            return map[key];
+        },
+
+        clear: function () {
+            map = {};
+        }
+
+    };
+};
